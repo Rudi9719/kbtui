73d9a0a0567877c49c6c4e61fc75ab703af51937
Gregory Rudolph <rudi9719@keybase.io>
2019-10-03 09:22:49 -0400

    Damnit

---
+++ cmdReact.go
@@ -0,0 +1,38 @@
+// +build !rm_basic_commands allcommands reactcmd
+
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	command := Command{
+		Cmd:         []string{"react", "r", "+"},
+		Description: "React to a message",
+		Help:        "",
+		Exec:        cmdUploadFile,
+	}
+
+	RegisterCommand(command)
+}
+
+func cmdReact(cmd []string) {
+	if len(cmd) == 3 {
+		reactToMessageId(cmd[1], cmd[2])
+	} else if len(cmd == 2) {
+		reactToMessage(cmd[1])
+	}
+
+}
+
+func reactToMessage(reaction string) {
+	chat := k.NewChat(channel)
+	chat.React(lastMessage.ID, reaction)
+
+}
+func reactToMessageId(messageId string, reaction string) {
+	chat := k.NewChat(channel)
+	ID, _ := strconv.Atoi(messageId)
+	chat.React(ID, reaction)
+}
