aa623828688b97d4c9b3132b3d6500ff917054e9
Gregory Rudolph <rudi9719@keybase.io>
2019-10-03 10:36:18 -0400

    Streaming mode back on

---
+++ cmdStream.go
@@ -0,0 +1,22 @@
+// +build !rm_basic_commands allcommands reactcmd
+
+package main
+
+
+func init() {
+	command := Command{
+		Cmd:         []string{"stream", "s" },
+		Description: "Stream all incoming messages",
+		Help:        "",
+		Exec:        cmdStream,
+	}
+
+	RegisterCommand(command)
+}
+
+func cmdStream(cmd []string) {
+	stream = true
+	channel.Name = ""
+	printToView("Feed", "You are now viewing the formatted stream")
+	clearView("Chat")
+}
