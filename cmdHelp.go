10fe8ce609da9fc6b4e3c2de8555b5caa50f6e87
Sam <dxb@keybase.io>
2019-10-04 15:30:12 -0400

    Show which type commands are loded

--- cmdHelp.go
+++ cmdHelp.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
 func init() {
@@ -20,10 +21,18 @@ func init() {
 
 func cmdHelp(cmd []string) {
 	var helpText string
+	var tCommands []string
 	if len(cmd) == 1 {
 		sort.Strings(baseCommands)
 		for _, c := range baseCommands {
 			helpText = fmt.Sprintf("%s%s%s\t\t%s\n", helpText, cmdPrefix, c, commands[c].Description)
+		}
+		if len(typeCommands) > 0 {
+			for c, _ := range typeCommands {
+				tCommands = append(tCommands, typeCommands[c].Name)
+			}
+			sort.Strings(tCommands)
+			helpText = fmt.Sprintf("%s\nThe following Type Commands are currently loaded: %s", helpText, strings.Join(tCommands, ", "))
 		}
 	}
 	printToView("Chat", helpText)
