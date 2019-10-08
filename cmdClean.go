12a5722d3b438cad42b14ea6e9bf7263b8de77c3
Gregory Rudolph <rudi9719@keybase.io>
2019-10-08 12:51:33 -0400

    New clean command for messed up windows

---
+++ cmdClean.go
@@ -0,0 +1,20 @@
+// +build !rm_basic_commands allcommands cleancmd
+
+package main
+
+func init() {
+	command := Command{
+		Cmd:         []string{"clean", "c"},
+		Description: "- Clean, or redraw chat view",
+		Help:        "",
+		Exec:        cmdClean,
+	}
+
+	RegisterCommand(command)
+}
+
+func cmdClean(cmd []string) {
+	clearView("Chat")
+	go populateChat()
+
+}
