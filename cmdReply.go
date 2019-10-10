156bbf6c7aa6393f3c0e437554791b682b54c27f
Gregory Rudolph <rudi9719@keybase.io>
2019-10-10 07:19:54 -0400

    Add cmdReply

---
+++ cmdReply.go
@@ -0,0 +1,30 @@
+// +ignore
+// +build allcommands replycmd
+
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func init() {
+	command := Command{
+		Cmd:         []string{"reply", "re"},
+		Description: "$messageId $response - Reply to a message",
+		Help:        "",
+		Exec:        cmdReply,
+	}
+
+	RegisterCommand(command)
+}
+
+func cmdReply(cmd []string) {
+	chat := k.NewChat(channel)
+	messageId, err := strconv.Atoi(cmd[1])
+	_, err = chat.Reply(messageId, strings.Join(cmd[2:], " "))
+	if err != nil {
+		printToView("Feed", "There was an error with your reply.")
+	}
+	return
+}
