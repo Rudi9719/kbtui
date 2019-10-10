2b018ebc765f1d90f2401d040e890f752a8f98e0
Gregory Rudolph <rudi9719@keybase.io>
2019-10-10 11:17:37 -0400

    Update chat if reactions occur: Prep for showing reactions in PS1

--- tcmdShowReactions.go
+++ tcmdShowReactions.go
@@ -21,10 +21,26 @@ func init() {
 
 func tcmdShowReactions(m keybase.ChatAPI) {
 	where := ""
+	team := false
 	if m.Msg.Channel.MembersType == keybase.TEAM {
+		team = true
 		where = fmt.Sprintf("in @%s#%s", m.Msg.Channel.Name, m.Msg.Channel.TopicName)
 	} else {
 		where = fmt.Sprintf("in a PM")
 	}
 	printToView("Feed", fmt.Sprintf("%s reacted to %d with %s %s", m.Msg.Sender.Username, m.Msg.Content.Reaction.M, m.Msg.Content.Reaction.B, where))
+	if channel.Name == m.Msg.Channel.Name {
+		if team {
+			if channel.TopicName == m.Msg.Channel.TopicName {
+				clearView("Chat")
+				go populateChat()
+			}
+			
+		} else {
+			clearView("Chat")
+			go populateChat()
+		}
+
+	}
+
 }
