f7a0368665b6cdf1d4834126c03cf0ba3350964e
Gregory Rudolph <rudi@nightmare.haus>
2019-10-17 09:38:22 -0400

    Feature: Allow relative paths for file upload

--- cmdUploadFile.go
+++ cmdUploadFile.go
@@ -4,12 +4,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func init() {
 	command := Command{
 		Cmd:         []string{"upload", "u"},
-		Description: "$filePath $fileName - Upload file with optional name",
+		Description: "$filePath $fileName - Upload file from absolute path with optional name",
 		Help:        "",
 		Exec:        cmdUploadFile,
 	}
@@ -19,6 +21,13 @@ func init() {
 
 func cmdUploadFile(cmd []string) {
 	filePath := cmd[1]
+	if !strings.HasPrefix(filePath, "/") {
+		dir, err := os.Getwd()
+		if err != nil {
+			printToView("Feed", fmt.Sprintf("There was an error determining path %+v", err))
+		}
+		filePath = fmt.Sprintf("%s/%s", dir, filePath)
+	}
 	var fileName string
 	if len(cmd) == 3 {
 		fileName = cmd[2]
@@ -28,7 +37,7 @@ func cmdUploadFile(cmd []string) {
 	chat := k.NewChat(channel)
 	_, err := chat.Upload(fileName, filePath)
 	if err != nil {
-		printToView("Feed", fmt.Sprintf("There was an error uploading %s to %s", filePath, channel.Name))
+		printToView("Feed", fmt.Sprintf("There was an error uploading %s to %s\n%+v", filePath, channel.Name, err))
 	} else {
 		printToView("Feed", fmt.Sprintf("Uploaded %s to %s", filePath, channel.Name))
 	}
